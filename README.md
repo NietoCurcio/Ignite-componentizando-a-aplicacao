2ed7cf062d1d64eab920b83430625d1d6326ab5f
Felipe Curcio <felipe_nieto010@hotmail.com>
2021-11-29 19:12:05 -0300

    desafio 02

---
+++ README.md
@@ -0,0 +1,5 @@
+# Ignite
+
+## Desafio 02 - Componentizando a aplicação
+
+Neste desafio a aplicação é separada em componentes, dessa forma, cada componente tem seu código isolado, melhorando a manutenção do projeto, pois cada componente é responsável por uma renderização específica.
--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,38 @@
-export function SideBar() {
-  // Complete aqui
+import { Button } from './Button'
+import { GenreResponseProps } from '../App'
+
+type SideBarProps = {
+  genres: GenreResponseProps[]
+  selectedGenreId: number
+  setSelectedGenreId: (arg: number) => void
+}
+
+export function SideBar({
+  genres,
+  setSelectedGenreId,
+  selectedGenreId,
+}: SideBarProps) {
+  function handleClickButton(id: number) {
+    setSelectedGenreId(id)
+  }
+
+  return (
+    <nav className="sidebar">
+      <span>
+        Watch<p>Me</p>
+      </span>
+
+      <div className="buttons-container">
+        {genres.map((genre: any) => (
+          <Button
+            key={String(genre.id)}
+            title={genre.title}
+            iconName={genre.name}
+            onClick={() => handleClickButton(genre.id)}
+            selected={selectedGenreId === genre.id}
+          />
+        ))}
+      </div>
+    </nav>
+  )
 }
